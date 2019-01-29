5125afc65cee17f03d052283fa775c5822f5f18a
Anton <aanohin98@gmail.com>
2019-01-29 20:29:22 +0300

    0.9.8
    Added class about__toggle, js script/

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-console.log('hello World!');
+$(document).ready(function() {
+
+	$('.about__toggle').click(function(event){
+
+		event.preventDefault();
+
+		console.log('Click!');
+
+		$('.about-content__box').hide();
+
+		var href = $(this).attr('href');
+
+		$(href).fadeIn();
+	});
+});
