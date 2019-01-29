82b46c3648339880d7e268a26f9f3cf866c35f59
Anton <aanohin98@gmail.com>
2019-01-29 21:15:53 +0300

    1.0
    Adaptive site. With button script and tabs.

--- js/main.js
+++ js/main.js
@@ -11,5 +11,40 @@ $(document).ready(function() {
 		var href = $(this).attr('href');
 
 		$(href).fadeIn();
+
 	});
+
+
+	var windowHeight = $(window).height();
+
+	console.log(windowHeight);
+
+
+		// следим за кнопкой (скроллом), показываем/скрываем 
+	$(window).scroll(function(){
+
+		console.log( $(this).scrollTop() );
+
+
+		if ( $(this).scrollTop() > windowHeight) {
+
+			$('#scrollToTop').fadeIn();
+		} else {
+
+			$('#scrollToTop').fadeOut();
+		}
+
+	});
+
+	// При клике на кнопку, делаем проекрутку на верх страницы
+
+	$('#scrollToTop').click(function(event){
+
+		event.preventDefault();
+
+		$('html').animate({scrollTop: 0}, 800)
+	})
+
+
+
 });
